f8c1830b3b242ff84069bfcc72e491a8a328bf9d
Akash-m15 <ashdragneel15@gmail.com>
2025-09-16 01:33:43 +0530

    Added Custom Rate Limiter in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,33 @@
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/middleware'
 
+
+const RATE_LIMIT = 15; 
+const TIME_WINDOW = 60 * 1000; 
+const ipMap = new Map<string, number[]>();
+
+
 export async function middleware(request: NextRequest) {
+  const ip = request.headers.get("x-forwarded-for")?.split(",")[0].trim() || "127.0.0.1";
+  const now = Date.now();
+
+  // Get existing timestamps or init new
+  const timestamps = ipMap.get(ip) || [];
+
+  // Filter out timestamps outside of the window
+  const recent = timestamps.filter(ts => now - ts < TIME_WINDOW);
+
+  if (recent.length >= RATE_LIMIT) {
+    return NextResponse.json(
+      { error: "Too many requests, slow down!" },
+      { status: 429 }
+    );
+  }
+  recent.push(now);
+  ipMap.set(ip, recent);
+
+  // return NextResponse.next();
+  
   return await updateSession(request)
 }
 
