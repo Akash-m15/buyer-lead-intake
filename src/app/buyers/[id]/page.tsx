ced5ceff6885c4b398816eaf66518620208dd799
Akash-m15 <ashdragneel15@gmail.com>
2025-09-20 15:07:37 +0530

    Removing unused var

--- src/app/buyers/[id]/page.tsx
+++ src/app/buyers/[id]/page.tsx
@@ -2,48 +2,45 @@ import { prisma } from "@/lib/db";
 import { createServerSupaBaseClient } from "@/lib/supabase/supabaseServer";
 import BuyerWrapper from "@/components/BuyerWrapper";
 
-export default async function getBuyerData({
-  params,
-}: {
+type PageProps = {
   params: { id: string };
-}) {
+};
+
+export default async function getBuyerData({ params }: PageProps) {
   try {
-    
     const supabase = await createServerSupaBaseClient();
     const {
       data: { user },
       error,
     } = await supabase.auth.getUser();
-    
+
     if (error || !user) {
       return <div>You must be logged in to view this page.</div>;
     }
 
-  const id = (await params).id;
-  const buyer = await getBuyer(id);
-  if(!buyer)
-  {
-    return <><div>No Buyer Found</div></>
+    const id = params.id;
+    const buyer = await getBuyer(id);
+    if (!buyer) {
+      return (
+        <>
+          <div>No Buyer Found</div>
+        </>
+      );
+    }
 
-  }
-    
-     return <BuyerWrapper initialBuyer={buyer} />
-    
+    return <BuyerWrapper initialBuyer={buyer} />;
   } catch (err) {
     console.log(err);
   }
 }
 
-
-async function getBuyer(id : string)
-{
+async function getBuyer(id: string) {
   const buyer = await prisma.buyer.findUnique({
     where: { id: id },
   });
 
   if (!buyer) {
-    return null
+    return null;
   }
-  return buyer
+  return buyer;
 }
-
